f2cd51265f44b230b270bfde3a9b7d53d90add85
majimearun <arunachala0619@gmail.com>
2023-04-06 15:44:38 +0530

    completed q6

---
+++ lab 9/06.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    int n, diff, elem, min = 9999999;
+    priority_queue<int> q1;
+    priority_queue<int> q2;
+    cin >> n >> diff;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> elem;
+        q1.push(elem);
+        if (elem < min)
+        {
+            min = elem;
+        }
+    }
+    int required = min + diff;
+    while (!q1.empty() && q1.top() >= required)
+    {
+        q2.push(q1.top());
+        q1.pop();
+    }
+    int count = 0;
+    while (!q1.empty() && !q2.empty())
+    {
+        if (q2.top() - q1.top() >= diff)
+        {
+            count++;
+            q2.pop();
+        }
+        q1.pop();
+    }
+    cout << count << endl;
+}
