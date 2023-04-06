b44064199fc53d66afeb2bed7226c23e02ca0fad
majimearun <arunachala0619@gmail.com>
2023-04-06 15:34:20 +0530

    completed q4

---
+++ lab 9/04.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    long long ans = 0;
+    map<long long, int> count;
+    count[0] = 1;
+    long long sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += a[i];
+        ans += count[sum - k * (i + 1)];
+        count[sum - k * (i + 1)]++;
+    }
+    cout << ans << endl;
+    return 0;
+}
