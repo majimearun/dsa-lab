1511c250a4adeec2e1a857f8366e29d3d12d2d8e
majimearun <arunachala0619@gmail.com>
2023-04-08 17:06:28 +0530

    2nd q completed

--- lab 9/02.cpp
+++ lab 9/02.cpp
@@ -1,59 +1,66 @@
-// Explanation
-// https://leetcode.com/problems/plates-between-candles/solutions/1586720/intuition-explained-prefix-sum-and-binary-search-c-clean-code/
-
-#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
+#include <stack>
+#include <vector>
 
 using namespace std;
-
-vector<int> plates_between_candles(string s, vector<vector<int>> &queries)
-{
-
-    int n = s.size();
-
-    vector<int> next_candle(n, n), prev_candle(n, -1), prefix_sum(n, 0);
-
-    for (int i = 0; i < n; i++)
-    {
-
-        prev_candle[i] = (s[i] == '*') ? ((i == 0) ? prev_candle[i] : prev_candle[i - 1]) : i;
-        next_candle[n - i - 1] =
-            (s[n - i - 1] == '*') ? ((i == 0) ? next_candle[n - i - 1] : next_candle[n - i]) : n - i - 1;
-
-        prefix_sum[i] = (i == 0) ? (s[i] == '*') : prefix_sum[i - 1] + (s[i] == '*');
-    }
-
-    vector<int> answer;
-
-    for (auto &query : queries)
-    {
-
-        int start = next_candle[query[0]] > query[1] ? 0 : next_candle[query[0]];
-        int end = prev_candle[query[1]] < query[0] ? 0 : prev_candle[query[1]];
-
-        answer.push_back(prefix_sum[end] - prefix_sum[start]);
-    }
-
-    return answer;
-}
 
 int main()
 {
     int n, q;
     cin >> n >> q;
-    string s;
-    cin >> s;
-    vector<vector<int>> queries;
-    for (int i = 0; i < q; i++)
+    int curr_candle = -1;
+    stack<int> plates_waiting;
+    // candle before, candle after
+    vector<vector<int>> search;
+    for (int i = 0; i < n; i++)
     {
-        int start, end;
-        cin >> start >> end;
-        queries.push_back({start, end});
+        search.push_back({-1, -1});
+    }
+    vector<int> prefix_sum(n, 0);
+    int count = 0;
+    char curr;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> curr;
+        if (curr == '|')
+        {
+            curr_candle = i;
+            while (!plates_waiting.empty())
+            {
+                search.at(plates_waiting.top()).at(1) = curr_candle;
+                plates_waiting.pop();
+            }
+            search.at(i) = {i, i};
+        }
+        else if (curr == '*')
+        {
+            search.at(i).at(0) = curr_candle;
+            plates_waiting.push(i);
+            count++;
+        }
+        prefix_sum.at(i) = count;
     }
 
-    auto answers = plates_between_candles(s, queries);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << search.at(i).at(0) << " " << search.at(i).at(1) << " " << prefix_sum.at(i) << endl;
+    // }
 
-    for (auto v : answers)
+    for (int i = 0; i < q; i++)
     {
-        cout << v << endl;
+        int l, r;
+        cin >> l >> r;
+        l = search.at(l).at(1);
+        r = search.at(r).at(0);
+        if (l == -1 || r == -1)
+        {
+            cout << 0 << endl;
+        }
+        else
+        {
+            cout << prefix_sum.at(r) - prefix_sum.at(l) << endl;
+        }
     }
 }
