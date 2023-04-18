fe71cb34b43d483884b12f4ebaf7156467415460
majimearun <arunachala0619@gmail.com>
2023-04-18 09:37:50 +0530

    feat: complete q10

--- lab 10/10.cpp
+++ lab 10/10.cpp
@@ -2,7 +2,45 @@
 
 using namespace std;
 
+int find_kth_smallest(vector<vector<int>> &v, int k)
+{
+    int n = v.size();
+    priority_queue<vector<int>, vector<vector<int>>, greater<>> min_heap;
+    for (int i = 0; i < n; i++)
+    {
+        min_heap.push({v[i][0], i, 0});
+    }
+    int ans{0};
+    for (int i = 0; i < k; i++)
+    {
+        auto top = min_heap.top();
+        min_heap.pop();
+        ans = top[0];
+        int row = top[1];
+        int col = top[2];
+        if (col + 1 < n)
+        {
+            min_heap.push({v[row][col + 1], row, col + 1});
+        }
+    }
+    return ans;
+}
+
 int main()
 {
+    int n, k;
+    cin >> n >> k;
+    vector<vector<int>> v(n, vector<int>(n));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> v[i][j];
+        }
+    }
+
+    cout << find_kth_smallest(v, k) << endl;
+
     return 0;
 }
