e2d6051730eee408b03e8db4420fd87f2275d802
majimearun <arunachala0619@gmail.com>
2023-04-18 10:37:31 +0530

    feat: sort the dolls, need to learn LIS to count

--- lab 10/05.cpp
+++ lab 10/05.cpp
@@ -2,7 +2,35 @@
 
 using namespace std;
 
+int max_nesting(vector<pair<int, int>> russian_dolls)
+{
+    int n = russian_dolls.size();
+    vector<int> dp(n, 1);
+    sort(russian_dolls.begin(), russian_dolls.end(), [](pair<int, int> a, pair<int, int> b) {
+        if (a.first == b.first)
+            return a.second > b.second;
+        return a.first < b.first;
+    });
+    for (int i = 0; i < n; i++)
+    {
+        cout << russian_dolls[i].first << " " << russian_dolls[i].second << endl;
+    }
+    return 0;
+}
+
 int main()
 {
+    vector<pair<int, int>> russian_dolls;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int w, h;
+        cin >> w >> h;
+        russian_dolls.push_back(make_pair(w, h));
+    }
+
+    cout << max_nesting(russian_dolls) << endl;
+
     return 0;
 }
